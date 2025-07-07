dfa3b92200a49120ffa04b83ec11ebf45b5d7342
eshwarigowda <86562118+eshwarigowda@users.noreply.github.com>
2025-07-08 00:09:44 +0530

    Add files via upload

---
+++ Day2Basicprogram/Program.cs
@@ -0,0 +1,31 @@
+﻿//importing sysytem
+using System;
+
+namespace firstProgram
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //Variable and datatype
+            string fname = "Eshwari";
+            int age = 20;
+            int myInt = 9;
+
+            //implicit casting
+            double mydouble = myInt;
+
+            //Explicit casting
+            int ageInFiveYears = age + (int)5.5;
+
+            //Output
+            Console.WriteLine("name:" + fname);
+            Console.WriteLine("age:" + age);
+            Console.WriteLine("double:" + mydouble);
+            Console.WriteLine("Age in 5 years :" + ageInFiveYears);
+            
+
+        }
+    }
+}
+
