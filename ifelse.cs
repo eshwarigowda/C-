a3f872628cd911c98f78932b351dbae7f3ac6679
eshwarigowda <86562118+eshwarigowda@users.noreply.github.com>
2025-07-07 15:55:32 +0530

    Update ifelse.cs

--- ifelse.cs
+++ ifelse.cs
@@ -3,7 +3,14 @@ public class Ifelse
 {
     public static void Run()
     {
+        int time = 20;
+        string result = (time < 18) ? "Good day." : "Good evening";
+        Console.WriteLine("short hand if else");
+        Console.WriteLine(result);
+
         int age = 25;
+        //if else
+        Console.WriteLine("ifElse");
         if (age > 18)
         {
             Console.WriteLine("Congratulations you are eligible for voting");
